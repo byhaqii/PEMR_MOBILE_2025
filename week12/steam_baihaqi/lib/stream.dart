e85ad256a810bd9cbab2e091321935d4022bdde7
Muhammad Rizal Al Baihaqi <rizalbaihaqiiiiii@gmail.com>
2025-11-23 15:10:18 +0700

    w12 : soal 2

---
+++ week12/steam_baihaqi/lib/stream.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> _colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.indigo,
+  ];
+}
