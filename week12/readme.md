f660bb649c0a45cf0a797b8756d265e73d52c17f
Muhammad Rizal Al Baihaqi <rizalbaihaqiiiiii@gmail.com>
2025-11-23 16:05:11 +0700

    w12 : soal 6 & 7

--- week12/readme.md
+++ week12/readme.md
@@ -1,1 +1,3 @@
 ![alt text](images/soal4.gif)
+
+![alt text](images/soal6.gif)
--- week12/steam_baihaqi/lib/stream.dart
+++ week12/steam_baihaqi/lib/stream.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ColorStream {
   final List<Color> _colors = [
@@ -14,6 +15,29 @@ class ColorStream {
     yield* Stream.periodic(const Duration(seconds: 1), (int t) {
       int index = t % _colors.length;
       return _colors[index];
+    });
+  }
+}
+
+class NumberStream {
+  final StreamController<int> controller = StreamController<int>();
+
+  void addNumberToSink(int newNumber) {
+    controller.sink.add(newNumber);
+  }
+
+  addError() {
+    controller.sink.addError('error');
+  }
+
+  close() {
+    controller.close();
+  }
+
+  // Menghasilkan angka bertambah setiap 500ms
+  Stream<int> getNumbers() async* {
+    yield* Stream.periodic(const Duration(milliseconds: 500), (int t) {
+      return t; // t akan mulai dari 0 dan bertambah
     });
   }
 }
